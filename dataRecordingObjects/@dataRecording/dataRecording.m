0f454c812823adbe980606e792d8e8f0cff54cfb
simon37robledo <simon37.robledo@gmail.com>
2025-02-04 11:24:46 +0200

    changes to getBombcell in dataRecording

--- dataRecordingObjects/@dataRecording/dataRecording.m
+++ dataRecordingObjects/@dataRecording/dataRecording.m
@@ -655,78 +655,173 @@ classdef (Abstract) dataRecording < handle
                 rerun = 0;
             end
 
+            %%%%%Set paths
+
+            %These paths below are the paths you will need to input to load data and 
+            % save the computed quality metrics / ephys properties. 
+            % All metrics relating to raw data (amplitude, signal to noise ratio) will not be computed. 
+
+            ephysKilosortPath = pathToKSresults;
+%             ephysRawFile = dir([pathToKSresults '*ap*.*bin']); % path to your raw .bin or .dat data
+%             ephysMetaDir = dir([pathToKSresults '*ap.meta']); % path to your .meta or .oebin meta file
+            savePath = [pathToKSresults 'qMetrics']; % where you want to save the quality metrics
+
             files = dir(pathToKSresults);
-
             fileNames = {files.name};
-
             APbin = char(fileNames(contains(fileNames,'tcat.imec0.ap.bin')));
             METAbin = char(fileNames(contains(fileNames,'tcat.imec0.ap.meta')));
-            
-            ephysRawDir = dir(fullfile(pathToKSresults,APbin)); % path to yourraw .bin or .dat data
+           
+            ephysRawFile = dir(fullfile(pathToKSresults,APbin)); % path to yourraw .bin or .dat data
             ephysMetaDir = dir(fullfile(pathToKSresults,METAbin)); % path to your .meta or .oebin meta file
-            saveLocation = pathToKSresults(1:strfind(pathToKSresults,'catgt')-2);
-            savePath = fullfile(saveLocation, 'qMetrics');
-            decompressDataLocal = saveLocation; % where to save raw decompressed ephys data
 
+            kilosortVersion = 4; % if using kilosort4, you need to have this value kilosertVersion=4. Otherwise it does not matter.
+            gain_to_uV = NaN; % use this if you are not using spikeGLX or openEphys to record your data. this value,
+            % when mulitplied by your raw data should convert it to  microvolts.
 
-            %%% load data
-            [spikeTimes_samples, spikeTemplates, templateWaveforms, templateAmplitudes, pcFeatures, ...
-                pcFeatureIdx, channelPositions] = bc_loadEphysData(pathToKSresults);
+            %%%%Load data
+            %This function loads are your ephys data. Use this function rather than any custom one
+            %as it handles zero-indexed values in a particular way. 
 
-            %%% detect whether data is compressed, decompress locally if necessary
-            rawFile = bc_manageDataCompression(ephysRawDir, decompressDataLocal);
+            [spikeTimes_samples, spikeClusters, templateWaveforms, templateAmplitudes, pcFeatures, ...
+                pcFeatureIdx, channelPositions] = bc.load.loadEphysData(ephysKilosortPath, savePath);
 
-            %%% which quality metric parameters to extract and thresholds
-            param = bc_qualityParamValues(ephysMetaDir, rawFile, pathToKSresults); %for unitmatch, run this:
-            % param = bc_qualityParamValuesForUnitMatch(ephysMetaDir, rawFile, ephysKilosortPath, gain_to_uV)
+            %%%%Run quality metrics
+            %%%%Set your paramaters.
+            %These define both how you will run quality metrics and how thresholds will be applied to quality metrics 
+            % to classify units into good/MUA/noise/non-axonal. 
+            % This function loads default, permissive values. 
+            % It's highly recommended for you to iteratively tweak these values to find values that suit your particular use case!
 
-%             param.firstPeakRatio = 1.3;
-%             param.minThreshDetectPeaksTroughs = 0.1;
-%             param.minThreshDetectPeaksTroughs = 
-% 
-%             param.minSpatialDecaySlope
+            param = bc.qm.qualityParamValues(ephysMetaDir, ephysRawFile, ephysKilosortPath, gain_to_uV, kilosortVersion);
 
+            %%%%Specific paremeters to tweek for lizards:
+            param.maxWvBaselineFraction = 0.4; %default value = 0.3
 
-           % param.maxWvBaselineFraction = 
+            %param.nChannels must correspond to the total number of channels in your raw data, including any sync channels. 
+            % For Neuropixels probes, this value should typically be either 384 or 385 channels. 
+            % param.nSyncChannels must correspond to the number of sync channels you recorded. This value is typically 1 or 0.
 
-            %%% compute quality metrics
-          
+            param.nChannels = 385;
+            param.nSyncChannels = 1;
+
+            % if using SpikeGLX, you can use this function:
+            if ~isempty(ephysMetaDir)
+                if endsWith(ephysMetaDir.name, '.ap.meta') %spikeGLX file-naming convention
+                    meta = bc.dependencies.SGLX_readMeta.ReadMeta(ephysMetaDir.name, ephysMetaDir.folder);
+                    [AP, ~, SY] = bc.dependencies.SGLX_readMeta.ChannelCountsIM(meta);
+                    param.nChannels = AP + SY;
+                    param.nSyncChannels = SY;
+                end
+            end
+
+            %%%%%Run all your quality metrics!
+            % This function runs all quality metrics, saves the metrics in your savePath folder and outputs some global summary 
+            % plots that can give you a good idea of how things went.
+
             qMetricsExist = ~isempty(dir(fullfile(savePath, 'qMetric*.mat'))) || ~isempty(dir(fullfile(savePath, 'templates._bc_qMetrics.parquet')));
 
             if qMetricsExist == 0 || rerun
-                [qMetric, unitType] = bc_runAllQualityMetrics(param, spikeTimes_samples, spikeTemplates, ...
-                    templateWaveforms, templateAmplitudes, pcFeatures, pcFeatureIdx, channelPositions, savePath);
+                [qMetric, unitType] = bc.qm.runAllQualityMetrics(param, spikeTimes_samples, spikeClusters, ...
+                templateWaveforms, templateAmplitudes, pcFeatures, pcFeatureIdx, channelPositions, savePath);
             else
-                [param, qMetric] = bc_loadSavedMetrics(savePath);
-                unitType = bc_getQualityUnitType(param, sortrows(qMetric,'maxChannels'), savePath);
+                [param, qMetric] = bc.load.loadSavedMetrics(savePath);
+                unitType = bc.qm.getQualityUnitType(param,qMetric,savePath);
+                %unitType = bc_getQualityUnitType(param, sortrows(qMetric,'maxChannels'), savePath);
             end
 
-
-            %%% view units + quality metrics in GUI
-            % load data for GUI
+            %%%%%Inspect
+            % After running quality metrics, espacially the first few times, it's a good idea to inspect your data and the quality metrics using the built-in GUI. Use your keyboard to navigate the GUI:
+            % left/right arrow : toggle between units
+            % u  : brings up a input dialog to enter the unit you want to go to
+            % g  : go to next good unit
+            % m : go to next multi-unit
+            % n  : go to next noise unit
+            % a  : go to next non-somatic unit ("a" is for axonal)
+            % up/down arrow : toggle between time chunks in the raw data
 
             if GUIbc == 1
-                loadRawTraces = 0; % default: don't load in raw data (this makes the GUI significantly faster)
-                bc_loadMetricsForGUI;
+                bc.load.loadMetricsForGUI;
 
-                % GUI guide:
-                % left/right arrow: toggle between units
-                % g : go to next good unit
-                % m : go to next multi-unit
-                % n : go to next noise unit
-                % up/down arrow: toggle between time chunks in the raw data
-                % u: brings up a input dialog to enter the unit you want to go to
-                unitQualityGuiHandle = bc_unitQualityGUI(memMapData, ephysData, qMetric, forGUI, rawWaveforms, ...
+                loadRawTraces = 0;
+                unitQualityGuiHandle = bc.viz.unitQualityGUI_synced(memMapData, ephysData, qMetric, forGUI, rawWaveforms, ...
                     param, probeLocation, unitType, loadRawTraces);
             end
-
-            %%% Save figures
-
-            if qMetricsExist == 0 || rerun
-                cd(savePath)
-                savefig(figure(1), 'templateWaveform.fig')
-                savefig(figure(2), 'qualityMetrics.fig')
-            end
+           
+% Previous BC run code 
+%             %%%%%% 
+% 
+%             files = dir(pathToKSresults);
+% 
+%             fileNames = {files.name};
+% 
+%             APbin = char(fileNames(contains(fileNames,'tcat.imec0.ap.bin')));
+%             METAbin = char(fileNames(contains(fileNames,'tcat.imec0.ap.meta')));
+%             
+%             ephysRawDir = dir(fullfile(pathToKSresults,APbin)); % path to yourraw .bin or .dat data
+%             ephysMetaDir = dir(fullfile(pathToKSresults,METAbin)); % path to your .meta or .oebin meta file
+%             saveLocation = pathToKSresults(1:strfind(pathToKSresults,'catgt')-2);
+%             savePath = fullfile(saveLocation, 'qMetrics');
+%             decompressDataLocal = saveLocation; % where to save raw decompressed ephys data
+% 
+% 
+%             %%% load data
+%             [spikeTimes_samples, spikeTemplates, templateWaveforms, templateAmplitudes, pcFeatures, ...
+%                 pcFeatureIdx, channelPositions] = bc_loadEphysData(pathToKSresults);
+% 
+%             %%% detect whether data is compressed, decompress locally if necessary
+%             rawFile = bc_manageDataCompression(ephysRawDir, decompressDataLocal);
+% 
+%             %%% which quality metric parameters to extract and thresholds
+%             param = bc_qualityParamValues(ephysMetaDir, rawFile, pathToKSresults); %for unitmatch, run this:
+%             % param = bc_qualityParamValuesForUnitMatch(ephysMetaDir, rawFile, ephysKilosortPath, gain_to_uV)
+% 
+% %             param.firstPeakRatio = 1.3;
+% %             param.minThreshDetectPeaksTroughs = 0.1;
+% %             param.minThreshDetectPeaksTroughs = 
+% % 
+% %             param.minSpatialDecaySlope
+% 
+% 
+%            % param.maxWvBaselineFraction = 
+% 
+%             %%% compute quality metrics
+%           
+%             qMetricsExist = ~isempty(dir(fullfile(savePath, 'qMetric*.mat'))) || ~isempty(dir(fullfile(savePath, 'templates._bc_qMetrics.parquet')));
+% 
+%             if qMetricsExist == 0 || rerun
+%                 [qMetric, unitType] = bc_runAllQualityMetrics(param, spikeTimes_samples, spikeTemplates, ...
+%                     templateWaveforms, templateAmplitudes, pcFeatures, pcFeatureIdx, channelPositions, savePath);
+%             else
+%                 [param, qMetric] = bc_loadSavedMetrics(savePath);
+%                 unitType = bc_getQualityUnitType(param, sortrows(qMetric,'maxChannels'), savePath);
+%             end
+% 
+% 
+%             %%% view units + quality metrics in GUI
+%             % load data for GUI
+% 
+%             if GUIbc == 1
+%                 loadRawTraces = 0; % default: don't load in raw data (this makes the GUI significantly faster)
+%                 bc_loadMetricsForGUI;
+% 
+%                 % GUI guide:
+%                 % left/right arrow: toggle between units
+%                 % g : go to next good unit
+%                 % m : go to next multi-unit
+%                 % n : go to next noise unit
+%                 % up/down arrow: toggle between time chunks in the raw data
+%                 % u: brings up a input dialog to enter the unit you want to go to
+%                 unitQualityGuiHandle = bc_unitQualityGUI(memMapData, ephysData, qMetric, forGUI, rawWaveforms, ...
+%                     param, probeLocation, unitType, loadRawTraces);
+%             end
+% 
+%             %%% Save figures
+% 
+%             if qMetricsExist == 0 || rerun
+%                 cd(savePath)
+%                 savefig(figure(1), 'templateWaveform.fig')
+%                 savefig(figure(2), 'qualityMetrics.fig')
+%             end
 
         end
 
